56c0be57519139e9177540d30cecaebc25442c5b
arsalan <arsalanmoid1@gmail.com>
2026-06-17 23:02:54 +0530

    use PORT env var for Render port binding

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -73,4 +74,5 @@ def ingest_all_endpoint():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
